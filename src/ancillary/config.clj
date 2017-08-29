21353298745d24a10ef806613afdba59032443ff
Tim Hawes <tim.hawes@inin.com>
2017-08-29 13:22:36 -0400

    progress

--- src/ancillary/config.clj
+++ src/ancillary/config.clj
@@ -3,7 +3,7 @@
 
 (defn read-config
   "Loads a config file in yaml format and returns a key-word vector."
-  ([] (read-config "/home/thawes/programs/etc/ancillary.yml"))
+  ([] (read-config "doc/example-ancillary.yml"))
   ([conf]
    (yaml/parse-string
     (slurp conf))))
