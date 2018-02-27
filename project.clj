9a7678ed448658b5c4d5ddf9f877b5e5ba723559
Tim Hawes <tim.hawes@genesys.com>
2018-02-26 21:07:35 -0500

    Correct the license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ancillary "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.fzakaria/slf4j-timbre "0.3.8"]
