0413a257a48410397d64b60021469182b008f089
Tim Hawes <tim.hawes@genesys.com>
2018-03-08 15:00:26 -0500

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ancillary "0.1.2-SNAPSHOT"
+(defproject ancillary "0.1.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "The MIT License"
